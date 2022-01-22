90d3f16a2213a49534c94bb89b87382189c4ec81
f180286 <f180286@cfd.nu.edu.pk>
2022-01-22 21:55:39 +0500

    Activity added in manifest

--- app/src/main/java/com/example/prayerlearner1/posture.java
+++ app/src/main/java/com/example/prayerlearner1/posture.java
@@ -5,37 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class posture extends AppCompatActivity {
-    Button bins, bins1;
+public class Posture extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posture);
-        bins =findViewById(R.id.button15);
-        bins.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                inspection();
-            }
-        });
-        bins1=findViewById(R.id.button17);
-        bins1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Demo();
-            }
-        });
     }
-    public void inspection() {
+
+    public void gotoposturecorrection(View view) {
         Intent intent=new Intent(this,posture2.class);
         startActivity(intent);
         finish();
     }
-    public void Demo() {
-        Intent intent=new Intent(this,posturedemo.class);
+    public void opendemo(View view) {
+        Intent intent=new Intent(this,postureDemo.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/prayerlearner1/posturedemo.java
+++ app/src/main/java/com/example/prayerlearner1/posturedemo.java
@@ -2,13 +2,23 @@ package com.example.prayerlearner1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class posturedemo extends AppCompatActivity {
+public class postureDemo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_posturedemo);
+        setContentView(R.layout.activity_posture_demo);
+
+
+    }
+    public void openposture(View view) {
+        Intent intent=new Intent(this,Posture.class);
+        startActivity(intent);
+        finish();
     }
 }
